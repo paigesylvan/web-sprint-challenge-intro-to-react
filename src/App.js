f3da2bcd05c0ae8f6391deae7481b107dc6540eb
Paige Sylvan <paigesylvan@gmail.com>
2023-07-16 16:38:17 -0500

    m

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useState} from 'react';
+//import axios from 'axios';
+
+import {data} from './mocks/handlers.js'
+console.log(data)
+
+//import Characters from './components/Character.js';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,12 +13,15 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [characters, setCharacters] = useState([data])
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+return (
+  <div className = 'app-list'>
+    <h1> {[characters.name]} </h1>
+    console.log({[characters.name]})
+  </div>
+)
+
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//1. Build a React component named 'Character' to render an individual character.
+//1. Map over the list in state, and for each character render a Character to the page.
+//1. Each rendered character must display its name in the DOM (e.g. "Luke Skywalker").
+//1. The character's name can't be hard-coded into the HTML. This data must be obtained from the API.
+//1. The components must be styled with **styled-components**.
+
+const Character = (props) => {
+
+const {characters} = props
+
+return (
+    <div className='individual-containter'>
+
+    </div>
+)
+}
+
+export default Character;
+
+//const dummyData = {
+//name: "Luke Skywalker",ß
+//gender: "male",
+//height: "172",
+//mass: "77",
+//birth_year: "19BBY",
+////eye_color: "blue",
+//hair_color: "blond",
+//skin_color: "fair",
+//}
